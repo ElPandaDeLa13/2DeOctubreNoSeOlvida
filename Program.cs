aa0e4c4e1b416563094ab3690fb28087be914817
cock <esteban.lopez193@tectijuana.edu.mx>
2020-03-11 11:51:47 -0800

    hola

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prueba
+{
+    class Prueba
+    {
+        public static void sumaUno(int x)
+        {
+            x = x+1;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=12;
+            Prueba.sumaUno(a);
+            Console.WriteLine(a);
+        }
+    }
+}
